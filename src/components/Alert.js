6ee0ea01999c964da229c351db363e7797632968
Prashant Chawla <98182593+1PrashantChawla@users.noreply.github.com>
2022-06-27 14:59:04 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function alert(props) {
   return (
-   props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
+   props.alert && <div className={`alert alert-${props.alert.type}`} style={{height:"15px"}} role="alert">
     {props.alert.alertMessage}
   </div>
   )
